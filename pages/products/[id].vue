f21d15190b5aa9c24044f5944af8e1ce635780b9
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2024-02-05 18:04:47 +0330

    Access the Route Params

---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Product {{ id }}</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem neque,
+      minus amet iusto beatae obcaecati cumque voluptatibus cupiditate ut
+      laudantium ratione! Ad exercitationem id aliquid distinctio neque tenetur
+      similique ab.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style lang="scss" scoped></style>
