6f2006602741db80657772cf6994576876e0cb04
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2024-02-05 19:32:33 +0330

    Adding style for layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
-  <div>
-    <nav class="flex justify-between">
-      <NuxtLink to="/">Logo</NuxtLink>
-      <ul>
+  <header class="shadow-sm bg-white">
+    <nav class="container mx-auto p-4 flex justify-between">
+      <NuxtLink class="font-bold" to="/">Nuxt.js Crash</NuxtLink>
+      <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/products">Products</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
       </ul>
     </nav>
-    <!-- output the page content -->
-    <div>
-      <slot />
-    </div>
+  </header>
+  <!-- output the page content -->
+  <div class="container mx-auto p-4">
+    <slot />
   </div>
 </template>
 
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <nav>
-      <NuxtLink to="/products">Logo with products</NuxtLink>
-    </nav>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4">
+        <NuxtLink class="font-bold" to="/products"
+          >Nuxt.js Crash - Products</NuxtLink
+        >
+      </nav>
+    </header>
     <!-- output the page content -->
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
 
-    <footer>
-      <ul>
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/products">Products</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
