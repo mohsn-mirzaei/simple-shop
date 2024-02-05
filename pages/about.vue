6b30aafd142226457660e1e2f0673e6ec9362489
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2024-02-05 17:58:24 +0330

    Adding pages

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+      laudantium dignissimos, quos eum quas adipisci sequi, dolore error beatae
+      cupiditate repellat odit blanditiis quidem fuga ratione fugiat porro
+      aliquid assumenda.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
