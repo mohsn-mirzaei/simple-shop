f507e4238b4b3959218ffd139ac2bdd882d99e04
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2024-02-05 19:36:06 +0330

    Use css inside tailwindCss

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
       <NuxtLink class="font-bold" to="/">Nuxt.js Crash</NuxtLink>
       <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/products">Products</NuxtLink></li>
+        <li><NuxtLink class="btn" to="/products">Products</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
       </ul>
     </nav>
